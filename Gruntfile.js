d2dc6010f0b03920d269e48a5ea6d2a44359b75d
micklove+github@gmail.com <micklove+github@gmail.com>
2016-02-07 23:16:26 +1100

    Add text area counter class example Add livereload with 'watch' plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,31 +20,30 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 
+		//https://github.com/gruntjs/grunt-contrib-connect
 		connect: {
-			myserver: {
-				port: 8000,
-				base: 'src/app',
-				hostname: '*',
-				onCreateServer: function(server, connect, options) {
-					grunt.log.writeln('SERVER STARTING');
-					var io = require('socket.io').listen(server);
-					io.sockets.on('connection', function(socket) {
-						// do something with socket
-					});
-				},
 
-				//options: {
-				//	port: 8000
-				//	base: 'src/app',
-				//	hostname: '*',
-				//	onCreateServer: function(server, connect, options) {
-				//		console.log("SERVER STARTING");
-				//		var io = require('socket.io').listen(server);
-				//		io.sockets.on('connection', function(socket) {
-				//			// do something with socket
-				//		});
-				//	}
-				//}
+
+			//grunt connect:keepalive, to prevent the webserver
+			//terminating when grunt does
+			myserver: {
+				options: {
+					port: 8080,
+					base: 'src/app',
+					livereload: true,
+					hostname: '*',
+					keepalive: false,
+					onCreateServer: function(server, connect, options) {
+						grunt.log.writeln('SERVER STARTING');
+
+						//http://socket.io/docs/
+						var io = require('socket.io').listen(server);
+						io.sockets.on('connection', function(socket) {
+							// do something with socket
+							socket.emit('gotconnection', { hello: 'world' });
+						});
+					},
+				}
 			},
 			myotherserver: {
 				port: 8001,
@@ -54,19 +53,78 @@ module.exports = function(grunt) {
 
 			}
 		},
+		//watch: {
+		//	html : {
+		//		files: ['src/**/*.html', '!_site/**/*.html'],
+		//	}
+		//	//files: ['<%= jshint.files %>'],
+		//	//tasks: ['jshint']
+		//	options: {
+		//		livereload: true
+		//	}
+		//},
 		watch: {
-			files: ['<%= jshint.files %>'],
-			tasks: ['jshint']
+			htmlfiles: {
+				// Replace with whatever file you want to trigger the update from
+				// Either as a String for a single entry
+				// or an Array of String for multiple entries
+				// You can use globing patterns like `css/**/*.css`
+				// See https://github.com/gruntjs/grunt-contrib-watch#files
+				files: [
+					'src/**/*.html',
+					'src/**/*.js',
+					'src/**/*.css',
+					'!bower_components/**/*.*'
+				],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		//open browser at project's URL
+		//https://www.npmjs.com/package/grunt-open
+		open: {
+			dev: {
+				// Gets the port from the connect configuration
+				//path: 'http://localhost:<%= express.all.options.port%>'
+				path: 'http://localhost:<%= connect.myserver.options.port%>/index.html'
+			},
+			prod: {
+				// Gets the port from the connect configuration
+				//path: 'http://localhost:<%= express.all.options.port%>'
+				path: 'http://someprodhost:<%= connect.options.port%>'
+			}
 		}
 	});
 
-	//grunt connect:keepalive, to prevent the webserver terminating when grunt does
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	//https://github.com/gruntjs/grunt-contrib-connect
-	grunt.loadNpmTasks('grunt-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	//grunt.registerTask('default', []);
+	// Load Grunt tasks declared in the package.json file
+	// (https://www.npmjs.com/package/matchdep)
+	require('matchdep')
+			.filterDev('grunt-*')
+			.forEach(grunt.loadNpmTasks);
+
+	//https://github.com/gruntjs/grunt-contrib-watch/blob/master/docs/watch-examples.md
+	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
+
+	grunt.event.on('gotconnection', function(action, filepath, target) {
+		//grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+		grunt.log.writeln("got connection");
+	});
+
 	grunt.registerTask('default', ['jshint']);
+
+	//live reload using 'watch' plugin
+	//http://rhumaric.com/2013/07/renewing-the-grunt-livereload-magic/
+
+	// Start web server
+	grunt.registerTask('serve', [
+		'connect:myserver',
+		'open:dev',
+		'watch:htmlfiles'
+	]);
 
 };
 
---
+++ src/app/core/react-textarea.js
@@ -0,0 +1,52 @@
+"use strict";
+/*global React, report, jslint */
+/*jslint nomen: true, debug: true*/
+
+var TextAreaCounter = React.createClass({
+	propTypes: {
+		defaultValue: React.PropTypes.string
+	},
+	getInitialState: function() { return {
+		text: this.props.defaultValue};
+	},
+	_textChange: function(ev) {
+		this._log('_textChange', ev);
+		this.setState({
+			text: ev.target.value
+		});
+	},
+	_log: function(methodName, args) {
+		console.log(methodName, args);
+	},
+	render:    function () {
+		//return React.DOM.div(null,
+		//	React.DOM.textarea({
+		//		value: this.state.text,
+		//		onChange: this._textChange
+		//	}),
+		//	React.DOM.h3(null, this.state.text.length));
+		return (
+				<div>
+					<textarea onChange={this._textChange} value={this.state.text}></textarea>
+					<div>{this.state.text.length}</div>
+				</div>
+		);
+	},
+	componentWillUpdate: function() {this._log('componentWillUpdate', arguments);},
+	componentDidUpdate: function()  {this._log('componentDidUpdate', arguments);},
+	componentWillMount: function()  {this._log('componentWillMount', arguments);},
+	componentDidMount: function()   {this._log('componentDidMount', arguments);},
+	componentWillUnmount: function()  {this._log('componentWillUnmount', arguments);}
+});
+
+//TODO - http://stackoverflow.com/questions/30730369/reactjs-component-not-rendering-textarea-with-state-variable
+
+var app = document.getElementById("app");
+		//ReactDOM.render(React.createElement(TextAreaCounter, {
+		//	defaultValue: "Hello World!"
+		//}), app);
+ReactDOM.render(
+		<div>
+			<TextAreaCounter defaultValue="Hello World!"/>
+		</div>,
+		app);
