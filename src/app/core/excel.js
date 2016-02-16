28c97ac473ee46c53f4c8a5a81107d6af8fe9093
micklove+github@gmail.com <micklove+github@gmail.com>
2016-02-17 00:56:01 +1100

    Added inline editing for fields, undo, redo

--- src/app/core/excel.js
+++ src/app/core/excel.js
@@ -3,21 +3,20 @@
 var Excel = React.createClass({
 
 	displayName: "Excel",
+	headers:     React.PropTypes.arrayOf(React.PropTypes.string),
+	initialData: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.string)),
+
 	getInitialState: function() {
 		return {
-			data: this.props.initialData,
+			//Use JSON methods to make a deep copy
+			data: JSON.parse(JSON.stringify(this.props.initialData)),
+			previousDataState: null,
 			sortby: null,
-			descending: false
+			descending: false,
+			edit: null //{row: index, cell: index}
 		};
 	},
-	headers: React.PropTypes.arrayOf(
-		React.PropTypes.string
-	),
-	initialData: React.PropTypes.arrayOf(
-		React.PropTypes.arrayOf(
-			React.PropTypes.string
-		)
-	),
+
 	_sort: function(e) {
 		var column = e.target.cellIndex;
 		var descending = this.state.sortby === column && !this.state.descending;
@@ -29,12 +28,6 @@ var Excel = React.createClass({
 			var valB = b[column];
 
 			return descending ? valA < valB : valA > valB;
-
-			/*
-			 descending
-			 ? a[column] < b[column]
-			 : a[column] > b[column];
-			 */
 		});
 
 		this.setState({
@@ -45,38 +38,119 @@ var Excel = React.createClass({
 	},
 
 	render: function() {
+		var self = this;
 		return (
+			React.DOM.div(null,
+
 			React.DOM.table(null,
 				React.DOM.thead({ onClick: this._sort},
 					React.DOM.tr(null,
-					this.props.headers.map(function(title, idx) {
+						this.props.headers.map(function(title, idx) {
 
-						if (this.state.sortby === idx) {
-							title += this.state.descending ? ' \u2191' : ' \u2193';
-						}
-						return React.DOM.th({key: idx}, title);
-					}.bind(this))
+							if (this.state.sortby === idx) {
+								title += this.state.descending ? ' \u2191' : ' \u2193';
+							}
+							return React.DOM.th({key: idx}, title);
+						}.bind(this))
 					)
 				),
-				React.DOM.tbody(null, this.state.data.map(function (row, idx) {
-					return ( React.DOM.tr({key: idx},
+				React.DOM.tbody({onDoubleClick: self._showEditor}, self.state.data.map(function (row, rowidx) {
+					return (
+						React.DOM.tr({key: rowidx},
 							row.map(function (cell, idx) {
-								return React.DOM.td({key: idx}, cell);
-							}) )
-					); })
-				)
-			)
-		);
+								var content = cell;
+
+								// if the `idx` and the `rowidx` match the one being edited
+								// otherwise just show the text content
+								var edit = self.state.edit;
+
+								if (edit && edit.row === rowidx && edit.cell === idx) { // ...
+									content = React.DOM.form({onSubmit: self._save}, React.DOM.input({
+											type: 'text',
+											defaultValue: content
+										})
+									);
+								}
+								return React.DOM.td({ key: idx,
+									'data-row': rowidx
+								}, content);
+							})
+						) );
+				}) )
+			), React.DOM.div(null,
+					React.DOM.button({onClick: function() {
+						self._undo();
+					}}, 'UNDO' ),
+					React.DOM.button({onClick: function() {
+						self._reset();
+					}}, 'RESET' ))
+		));
+	},
+
+	_savePreviousState: function () {
+		this.setState({
+			edit: null, // done editing
+			previousDataState: JSON.parse(JSON.stringify(this.state.data))
+		});
+
+	},
+
+	_undo: function () {
+		this._savePreviousState();
+		this.setState({
+			edit: null, // done editing
+			data: JSON.parse(JSON.stringify(this.state.previousDataState))
+		});
+	},
+
+	_reset: function () {
+		this._savePreviousState();
+
+		this.setState({
+			edit: null, // done editing
+			data: JSON.parse(JSON.stringify(this.props.initialData))
+		});
+
+	},
+
+	_save: function (e) {
+		e.preventDefault();
+
+		//
+		var input = e.target.firstChild;
+		var data = this.state.data.slice();
+
+		//Save previous state, so that we can undo
+		this._savePreviousState();
+		data[this.state.edit.row][this.state.edit.cell] = input.value;
+		this.setState({
+			edit: null, // done editing
+			data: data
+		});
+	},
+
+	_showEditor: function (e) {
+		var row = parseInt(e.target.dataset.row, 10);
+		var cell = e.target.cellIndex;
+
+		this.setState({
+			edit: {
+				row:  row,
+				cell: cell
+			}
+		});
+		//alert('row: ' + row + '\n' + 'cell: ' + cell);
 	}
+
 });
 
 var headers = [
 	"Book", "Author", "Language", "Published", "Sales"
 ];
 
-var data = [
+var startData = [
 	["The Lord of the Rings",                    "J. R. R. Tolkien",         "English", "1954–1955", "150 million"],
-	["Le Petit Prince (The Little Prince)",      "Antoine de Saint-Exupéry", "French", "1943",       "140 million"],
+	["Le Petit Prince (The Little Prince)",      "Antoine de Saint-Exupéry", "French",  "1943",      "140 million"],
 	["Harry Potter and the Philosopher's Stone", "J. K. Rowling",            "English", "1997",      "107 million"],
 	["And Then There Were None",                 "Agatha Christie",          "English", "1939",      "100 million"],
 	["Dream of the Red Chamber",                 "Cao Xueqin",               "Chinese", "1754–1791", "100 million"],
@@ -86,5 +160,5 @@ var data = [
 
 ReactDOM.render(React.createElement(Excel, {
 	headers: headers,
-	initialData: data
+	initialData: startData
 }), document.getElementById("app"));
